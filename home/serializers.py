2c2b6162d7f8c0468e9c8b48d4fb47e966887003
Chintan K <chintank8080@gmail.com>
2023-05-20 00:14:35 +0530

    comment remove

--- home/serializers.py
+++ home/serializers.py
@@ -4,13 +4,11 @@ from django.contrib.auth.models import User
 
 
 
-# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
 
-# Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
